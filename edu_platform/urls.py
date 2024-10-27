369a6307a884b1c5ea3ef5b60b4cdb08baf42cec
Bakkenti <baktiarlesov@gmail.com>
2024-10-28 00:20:24 +0600

    Changes

--- edu_platform/urls.py
+++ edu_platform/urls.py
@@ -5,7 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('select2/', include('django_select2.urls')),
     path('', include('myapp.urls')),
 ]
 
